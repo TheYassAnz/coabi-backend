890b2682832918d06729723c951cdbdef12abd48
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2025-03-17 17:36:49 +0100

    chore: prettier init

--- server.ts
+++ server.ts
@@ -1,16 +1,14 @@
-import dotenv from 'dotenv';
-import express, { Application, Request, Response } from 'express';
+import dotenv from "dotenv";
+import express, { Application, Request, Response } from "express";
 
 dotenv.config();
 const app: Application = express();
 const PORT: string | number = process.env.POSTGRES_PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
