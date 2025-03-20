6c1468e820458e1205ea6ea36e78a35ec8d6202b
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2025-03-20 11:47:15 +0100

    chore: structure update and mongoose init

--- server.ts
+++ server.ts
@@ -1,14 +1,12 @@
+import app from "./src/app";
 import dotenv from "dotenv";
-import express, { Application, Request, Response } from "express";
 
+// Load environment variables
 dotenv.config();
-const app: Application = express();
-const PORT: string | number = process.env.POSTGRES_PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+const PORT: string | number = process.env.API_PORT || 8080;
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+import mongoose from "mongoose";
+
+const clientOptions = {
+  serverApi: { version: "1" as const, strict: true, deprecationErrors: true },
+};
+
+mongoose
+  .connect(`${process.env.MONGODB_URI}`, clientOptions)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB", error);
+  });
+
+const app: Application = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Hello World!" });
+});
+
+export default app;
