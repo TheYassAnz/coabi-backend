cc82e3d183c3078aa81b128987df5db221172d6d
Abubakar Aliev <aliev_a@etna-alternance.net>
2025-03-29 20:58:37 +0100

    feat(controller): add getOneEvent function to retrieve an event by ID

--- src/controllers/event.ts
+++ src/controllers/event.ts
@@ -11,3 +11,24 @@ export const getAllEvents = async (req: Request, res: Response) => {
         res.status(500).json({ error: "Une erreur est survenue lors de la récupération des événements." });
     }
 };
+
+// Récupérer un événement par ID
+export const getOneEvent = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: "L'ID fourni n'est pas valide." });
+        }
+
+        const event = await Event.findById(id);
+        
+        if (!event) {
+            return res.status(404).json({ message: "Événement non trouvé." });
+        }
+
+        res.status(200).json(event);
+    } catch (error) {
+        res.status(500).json({ error: "Une erreur est survenue lors de la récupération de l'événement." });
+    }
+};
