738f5474f09dbf78d8375590f64cf9ecf0be5acf
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2025-03-29 23:57:44 +0100

    fix(user): update field names firstname and username

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,8 +1,8 @@
-import { Schema, model, Types, Document } from 'mongoose';
+import { Schema, model, Types, Document } from "mongoose";
 
 interface User extends Document {
-  first_name: string;
-  last_name: string;
+  firstname: string;
+  lastname: string;
   username: string;
   password: string;
   age: number;
@@ -10,22 +10,30 @@ interface User extends Document {
   email: string;
   phone_number: string;
   profile_picture_id: Types.ObjectId | null;
-  accommodation_id: Types.ObjectId | null; 
-};
+  accommodation_id: Types.ObjectId | null;
+}
 
 const userSchema = new Schema<User>({
-  first_name: { type: String, required: true },
-  last_name: { type: String, required: true },
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   age: { type: Number, required: true },
   description: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   phone_number: { type: String, required: true },
-  profile_picture_id: { type: Schema.Types.ObjectId, ref: 'File', required: false },
-  accommodation_id: { type: Schema.Types.ObjectId, ref: 'Accommodation', required: false }
+  profile_picture_id: {
+    type: Schema.Types.ObjectId,
+    ref: "File",
+    required: false,
+  },
+  accommodation_id: {
+    type: Schema.Types.ObjectId,
+    ref: "Accommodation",
+    required: false,
+  },
 });
 
-const UserModel = model<User>('User', userSchema);
+const UserModel = model<User>("User", userSchema);
 
 export default UserModel;
