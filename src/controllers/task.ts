fc0ad9180981b6bcc5dc0d1b40223106edcd08f3
Abubakar Aliev <aliev_a@etna-alternance.net>
2025-03-30 11:06:19 +0200

    feat(task): add controller to retrieve a single task by ID

--- src/controllers/task.ts
+++ src/controllers/task.ts
@@ -13,9 +13,31 @@ const getAllTasks = async (req: Request, res: Response) => {
   }
 };
 
+const getOneTask = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      res.status(400).json({ message: "L'ID fourni n'est pas valide." });
+    }
+
+    const task = await Task.findById(id);
+
+    if (!task) {
+      res.status(404).json({ message: "Tâche non trouvée." });
+    }
+
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({
+      error: "Une erreur est survenue lors de la récupération de la tâche.",
+    });
+  }
+};
+
 export default {
   getAllTasks,
-  //   getOneTask,
+  getOneTask,
   //   createTask,
   //   updateTask,
   //   deleteTask,
