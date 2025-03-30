c35ad7f05ede964e88a82791eafa62ccc8454ca3
Abubakar Aliev <aliev_a@etna-alternance.net>
2025-03-30 11:09:15 +0200

    feat(task): add deleteTask controller for task deletion

--- src/controllers/task.ts
+++ src/controllers/task.ts
@@ -89,10 +89,32 @@ const updateTask = async (req: Request, res: Response) => {
 
     res.status(200).json(task);
   } catch (error) {
+    res.status(500).json({
+      error: "Une erreur est survenue lors de la mise à jour de la tâche.",
+    });
+  }
+};
+
+const deleteTask = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "L'ID fourni n'est pas valide." });
+    }
+
+    const task = await Task.findByIdAndDelete(id);
+
+    if (!task) {
+      return res.status(404).json({ message: "Tâche non trouvée." });
+    }
+
+    res.status(200).json({ message: "Tâche supprimée avec succès." });
+  } catch (error) {
     res
       .status(500)
       .json({
-        error: "Une erreur est survenue lors de la mise à jour de la tâche.",
+        error: "Une erreur est survenue lors de la suppression de la tâche.",
       });
   }
 };
@@ -102,5 +124,5 @@ export default {
   getOneTask,
   createTask,
   updateTask,
-  //   deleteTask,
+  deleteTask,
 };
