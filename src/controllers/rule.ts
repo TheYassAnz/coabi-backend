43e1c04d20b09eddef28d18134b8faca3704b11a
Abubakar Aliev <aliev_a@etna-alternance.net>
2025-03-30 10:12:46 +0200

    feat(rule): implement getOneRule controller with error handling

--- src/controllers/rule.ts
+++ src/controllers/rule.ts
@@ -7,17 +7,39 @@ const getAllRules = async (req: Request, res: Response) => {
     const rules = await Rule.find();
     res.status(200).json({ rules });
   } catch (error) {
+    res.status(500).json({
+      error: "Une erreur est survenue lors de la récupération des règles.",
+    });
+  }
+};
+
+const getOneRule = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "L'ID fourni n'est pas valide." });
+    }
+
+    const rule = await Rule.findById(id);
+
+    if (!rule) {
+      return res.status(404).json({ message: "Règle non trouvée." });
+    }
+
+    res.status(200).json(rule);
+  } catch (error) {
     res
       .status(500)
       .json({
-        error: "Une erreur est survenue lors de la récupération des règles.",
+        error: "Une erreur est survenue lors de la récupération de la règle.",
       });
   }
 };
 
 export default {
   getAllRules,
-  // getOneRule,
+  getOneRule,
   // createRule,
   // updateRule,
   // deleteRule
