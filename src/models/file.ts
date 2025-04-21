dc681836929535c1b82f52c2475a872459e7b97b
eap_g <kentucky@GUI>
2025-04-21 16:25:14 +0200

    refactor: description field in models except rule model

--- src/models/file.ts
+++ src/models/file.ts
@@ -1,27 +1,27 @@
-import { Schema, model, Types, Document } from 'mongoose';
+import { Schema, model, Types, Document } from "mongoose";
 
 export enum FileTypes {
-  IMAGE = 'image',
-  PDF = 'pdf'
+  IMAGE = "image",
+  PDF = "pdf",
 }
 
 interface File extends Document {
   _id: string;
   name: string;
-  description: string;
+  description: string | null;
   type: FileTypes;
   size: number;
-  user_id: Types.ObjectId; 
-};
+  user_id: Types.ObjectId;
+}
 
 const fileSchema = new Schema<File>({
   _id: { type: String, required: true },
-  description: { type: String, required: true },
+  description: { type: String, required: false, default: null },
   type: { type: String, enum: Object.values(FileTypes), required: true },
   size: { type: Number, required: true },
-  user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true }
+  user_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
 });
 
-const FileModel = model<File>('File', fileSchema);
+const FileModel = model<File>("File", fileSchema);
 
 export { FileModel };
--- src/models/task.ts
+++ src/models/task.ts
@@ -1,23 +1,27 @@
-import { Schema, model, Types, Document } from 'mongoose';
+import { Schema, model, Types, Document } from "mongoose";
 
 interface Task extends Document {
   name: string;
-  description: string;
+  description: string | null;
   weekly: boolean;
   done: boolean;
   user_id: Types.ObjectId;
-  accommodation_id: Types.ObjectId; 
-};
+  accommodation_id: Types.ObjectId;
+}
 
 const taskSchema = new Schema<Task>({
   name: { type: String, required: true },
-  description: { type: String, required: true },
+  description: { type: String, required: false, default: null },
   weekly: { type: Boolean, required: true },
   done: { type: Boolean, required: true },
-  user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true  },
-  accommodation_id: { type: Schema.Types.ObjectId, ref: 'Accommodation', required: true }
+  user_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  accommodation_id: {
+    type: Schema.Types.ObjectId,
+    ref: "Accommodation",
+    required: true,
+  },
 });
 
-const TaskModel = model<Task>('Task', taskSchema);
+const TaskModel = model<Task>("Task", taskSchema);
 
 export default TaskModel;
