0a31b5ef626a89907256ef6b81aa9190ceb0e268
eap_g <kentucky@GUI>
2025-04-24 17:46:44 +0200

    feat: add refresh token with csrf token

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,8 +1,13 @@
 import User from "../models/user";
 import { Request, Response } from "express";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 import { validPasswordLength } from "../utils/utils";
+import {
+  generateTokens,
+  setRefreshTokenCookie,
+  verifyRefreshToken,
+} from "../utils/auth/jwt";
+import { csrfTokens, generateCsrfToken } from "../utils/auth/csrf";
 
 const register = async (req: Request, res: Response): Promise<any> => {
   try {
@@ -33,7 +38,7 @@ const register = async (req: Request, res: Response): Promise<any> => {
   }
 };
 
-const login = async (req: Request, res: Response) => {
+const login = async (req: Request, res: Response): Promise<any> => {
   try {
     const { username, password } = req.body;
 
@@ -56,9 +61,68 @@ const login = async (req: Request, res: Response) => {
       return;
     }
 
-    const secretKey = process.env.SECRET_KEY || "zDZEKIOazirnaz";
-    const token = jwt.sign({ id: user._id }, secretKey, { expiresIn: "1h" });
-    res.status(200).json({ message: "OK", data: token });
+    const { accessToken, refreshToken } = generateTokens({
+      id: user._id.toString(),
+    });
+    setRefreshTokenCookie(res, refreshToken);
+
+    res.status(200).json({ message: "OK", data: accessToken });
+  } catch (error: any) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const csrf = async (req: Request, res: Response): Promise<any> => {
+  try {
+    const secret = await csrfTokens.secret();
+    const token = await generateCsrfToken(secret);
+
+    res.cookie("csrfSecret", secret, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      path: "/",
+      maxAge: 60 * 60 * 1000, // 1 hour
+    });
+
+    return res.status(200).json({ data: token });
+  } catch (error) {
+    return res.status(403).json({ error: "Invalid csrf token" });
+  }
+};
+
+const refresh = async (req: Request, res: Response): Promise<any> => {
+  try {
+    const refreshToken = req.cookies.refreshToken;
+
+    if (!refreshToken) {
+      return res.status(401).json({ error: "No refresh token provided" });
+    }
+
+    const decoded = verifyRefreshToken(refreshToken);
+
+    if (!decoded) {
+      return res
+        .status(401)
+        .json({ message: "Invalid or expired refresh token" });
+    }
+
+    const userId = typeof decoded === "string" ? decoded : decoded.id;
+    const tokens = generateTokens({
+      id: userId,
+    });
+
+    setRefreshTokenCookie(res, tokens.refreshToken);
+    return res.status(200).json({ data: tokens.accessToken });
+  } catch (error: any) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+const logout = async (req: Request, res: Response): Promise<any> => {
+  try {
+    res.clearCookie("refreshToken");
+    return res.sendStatus(204);
   } catch (error: any) {
     res.status(500).json({ message: "Internal server error" });
   }
@@ -67,4 +131,7 @@ const login = async (req: Request, res: Response) => {
 export default {
   register,
   login,
+  csrf,
+  refresh,
+  logout,
 };
